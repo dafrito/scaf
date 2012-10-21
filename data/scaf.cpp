0f2f2f585844b2fdebd032dfba454fabab87e5b6
Aaron Faanes <dafrito@gmail.com>
2012-10-20 21:00:30 -0500

    Added C++ scaffolds

---
+++ data/scaf.cpp
@@ -0,0 +1,5 @@
+#include "@HEADER@"
+
+// TODO Insert code here.
+
+// vim: set ts=4 sw=4 et :
---
+++ data/scaf.hpp
@@ -0,0 +1,8 @@
+#ifndef @HEADER_DEFINE@
+#define @HEADER_DEFINE@
+
+// TODO Insert header code here.
+
+#endif // @HEADER_DEFINE@
+
+// vim: set ts=4 sw=4 et :
