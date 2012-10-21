21e95d730919af1fef5dbc959d7da280add903c2
Aaron Faanes <dafrito@gmail.com>
2012-10-21 02:18:28 -0500

    qtmain uses PROJECT override

--- data/qtmain.cpp
+++ data/qtmain.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QMainWindow gui;
-    gui.setWindowTitle("Hello, world!");
+    gui.setWindowTitle("@PROJECT@");
     centerApp(gui);
 
     QPushButton quit("Quit", &gui);
