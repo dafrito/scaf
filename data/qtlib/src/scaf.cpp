238160fca24447e9f0769363b6dae824b48cde56
Aaron Faanes <dafrito@gmail.com>
2013-04-04 04:32:09 -0500

    Removed qtlib, alias to qtapp

--- data/qtlib/src/scaf.cpp
+++
@@ -1,6 +0,0 @@
-#include <@@PROJECT_OBJ@@.hpp>
-
-int @@PROJECT_OBJ@@::add(const int a, const int b) const
-{
-    return a + b;
-}
--- data/qtlib/tests/basic.cpp
+++
@@ -1,7 +0,0 @@
-#include "init.hpp"
-
-BOOST_AUTO_TEST_CASE(additionWorks)
-{
-    @@PROJECT_OBJ@@ obj;
-    BOOST_REQUIRE_EQUAL(4, obj.add(2, 2));
-}
